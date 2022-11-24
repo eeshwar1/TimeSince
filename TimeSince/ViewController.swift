b667c4244e5a5f1edc8eebfda84688238beaa5f3
Venky Venkatakrishnan <eeshwar1@gmail.com>
2022-11-24 22:04:24 +0530

    Initial Commit

---
+++ TimeSince/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TimeSince
+//
+//  Created by Venkateswaran Venkatakrishnan on 11/24/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
