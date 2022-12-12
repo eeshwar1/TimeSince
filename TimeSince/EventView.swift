7540d2005b8ad0682dd178e8872241988c30842e
Venky Venkatakrishnan <eeshwar1@gmail.com>
2022-12-11 19:18:31 -0600

    Several format changes to fix positions of fields

--- TimeSince/EventView.swift
+++ TimeSince/EventView.swift
@@ -15,58 +15,63 @@ struct EventView: View {
         
         GeometryReader { (geometry) in
             
-            HStack {
-                
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text(event.name ?? "Invalid")
-                            .font(.largeTitle)
-                            .bold()
-                            .frame(minWidth: 300, alignment: .leading)
-                        Spacer()
-                            .frame(minWidth: 10)
-                        Text(event.timeSince)
-                            .foregroundColor(.blue)
-                            .font(.body)
-                            .frame(minWidth: 100)
-                    }
-                    HStack(alignment: .center) {
-                        Text(event.dateValue).foregroundColor(.black)
-                            .font(.subheadline)
-                            .frame(width: 100, alignment: .leading)
-                        Spacer()
-                            .frame(minWidth: 50)
-                   
-                    }
-                    HStack(alignment: .center) {
-                        Text(event.id.uuidString).foregroundColor(.gray)
-                            .font(.subheadline)
-                            .frame(width: 300, alignment: .leading)
-                        Spacer()
-                            .frame(minWidth: 50)
-                    }
-                   
-                }
-                .padding(20)
-                .frame(width: geometry.size.width - 60,
-                       height: geometry.size.height, alignment: .leading)
-                
-                
-                
-               
-            }
+           
+                HStack {
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(event.name ?? "Invalid")
+                                .font(.largeTitle)
+                                .bold()
+                                .frame(minWidth: 200, alignment: .leading)
+                            Spacer()
+                                .frame(minWidth: 10)
+                            Text(event.timeSince)
+                                .foregroundColor(.blue)
+                                .font(.body)
+                                .frame(minWidth: 100)
+                        }
+                        HStack(alignment: .center) {
+                            Text(event.dateValue).foregroundColor(.black)
+                                .font(.subheadline)
+                                .frame(width: 100, alignment: .leading)
+                            Spacer()
+                                .frame(minWidth: 50)
                             
+                        }
+                        HStack(alignment: .center) {
+                            Text(event.id.uuidString).foregroundColor(.gray)
+                                .font(.subheadline)
+                                .frame(width: 300, alignment: .leading)
+                            Spacer()
+                                .frame(minWidth: 50)
+                        }
+                        
+                    }
+                    .padding(20)
+                    .frame(width: geometry.size.width - 40,
+                           height: 100, alignment: .leading)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 4)
+                    )
+                    
+                    
+                }
                 
                 
-        }.padding(.bottom, 10)
-            
+            }
+        .padding(.bottom, 45)
+        .padding(.leading, 10)
         
-    }
+        }
+        
+       
 }
 
 struct EventView_Previews: PreviewProvider {
     static var previews: some View {
-        EventView(event: Event(name: "Hello World", date: "10/01/2022"))
+        EventView(event: Event(name: "Demo Event", date: "10/01/2022"))
 
     }
 }
