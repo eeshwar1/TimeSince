be7aca57c9d4c1b827aacebe69e534df2cae1ee7
Venky Venkatakrishnan <eeshwar1@gmail.com>
2022-12-12 21:27:44 -0600

    added sort by field functionality via buttons

--- TimeSince/EventListView.swift
+++ TimeSince/EventListView.swift
@@ -13,6 +13,9 @@ struct EventListView: View {
     
     @State var addNew: Bool = false
     
+    @State var sortedBy: String = "date"
+    @State var ascendingOrder: Bool = false
+    
     var controller: ViewController?
     
     var body: some View {
@@ -34,7 +37,11 @@ struct EventListView: View {
                         .frame(width: 50, height: 50)
                         .background(Color.blue).cornerRadius(10)
                         .keyboardShortcut("n")
-                         
+                        
+                            
+                        SortButton(field: "name")
+                        SortButton(field: "date")
+
                         
                     }
                     .padding(10)
@@ -70,6 +77,20 @@ struct EventListView: View {
         
     }
         
+    func executeSort(by field: String) {
+        
+//        print("executeSort")
+        
+        self.sortedBy = field
+        
+        self.ascendingOrder.toggle()
+        
+        if let controller = controller {
+            
+            controller.sortEvents(by: self.sortedBy, ascendingOrder: ascendingOrder)
+        }
+        
+    }
         
 }
 
@@ -82,7 +103,7 @@ extension EventListView {
             
             VStack(alignment: .center, spacing: 50) {
                 
-                ForEach(eventList.events, id: \.id) { event in
+                ForEach(eventList.getEvents(), id: \.id) { event in
                     VStack {
                         HStack() {
                             EventView(event: event)
@@ -98,19 +119,54 @@ extension EventListView {
                                     .background(Color.red)
                                     .foregroundColor(Color.white)
                             }
-                                    .frame(width: 50, height: 50)
+                                                                .frame(width: 50, height: 50)
                                     .background(Color.red).cornerRadius(10)
                              
                         }
                     }
                 }
             }
-            .padding(.init(top: 20, leading: 0, bottom: 50, trailing: 0))
+            .padding(.init(top: 20, leading: 10, bottom: 50, trailing: 10))
              
         }
-        .padding()
+        .padding(10)
         .scrollDisabled(false)
          
+    }
+    
+    func SortButton(field: String) -> some View {
+        
+        
+        return Button {
+            executeSort(by: field)
+            
+        } label: {
+            
+            if (sortedBy == field) {
+                
+                
+                Label("Sort by " + field, systemImage: ascendingOrder ? "arrow.up": "arrow.down")
+                    .bold()
+                    .padding(10)
+                    .frame(width: 150, height: 50)
+                    .background(Color.orange)
+                    .foregroundColor(Color.white)
+            } else {
+                
+                Text("Sort by " + field)
+                    .bold()
+                    .padding(10)
+                    .frame(width: 150, height: 50)
+                    .background(Color.orange)
+                    .foregroundColor(Color.white)
+            }
+            
+            
+        }
+       .frame(width: 150, height: 50)
+       .background(Color.orange).cornerRadius(10)
+       .keyboardShortcut("s")
+        
     }
     
 }
