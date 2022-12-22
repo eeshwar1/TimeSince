7b10b752f745f3609907102923c64dbba75fb07e
Venky Venkatakrishnan <eeshwar1@gmail.com>
2022-12-22 17:21:03 -0600

    Modified to handle Edit mode in addition to View mode, several changes that alter this View drastically

--- TimeSince/EventView.swift
+++ TimeSince/EventView.swift
@@ -9,40 +9,120 @@ import SwiftUI
 
 struct EventView: View {
     
-    var event: Event
+    @State var event: Event
     
-    @State var fgPrimaryColor = Color.white
-    @State var fgAccentColor = Color.yellow
+    // TODO: Colors do not work well in Dark Mode. Text is not readable
+    
+    @State var fgPrimaryColor = Color.primary
+    @State var fgAccentColor = Color.accentColor
+    
+    @State var inEditMode:  Bool = false
+    
+    @State var showingDeleteConfirmation: Bool = false
+    
+    var controller: ViewController?
     
     var body: some View {
         
         GeometryReader { (geometry) in
             
            
-                HStack {
-                    
+            HStack {
+                
+                ScrollView {
                     VStack(alignment: .leading) {
                         HStack {
-                            Text(event.name)
-                                .font(.largeTitle)
-                                .bold()
-                                .frame(minWidth: 200, alignment: .leading)
-                                .foregroundColor(fgPrimaryColor)
-                            Spacer()
-                                .frame(minWidth: 10)
-                            Text(event.daysSince)
-                                .foregroundColor(fgAccentColor)
-                                .font(.body)
-                                .frame(minWidth: 100)
-                        }
-                        HStack(alignment: .center) {
-                            Text(event.dateValue).foregroundColor(fgPrimaryColor)
-                                .font(.subheadline)
-                                .frame(width: 100, alignment: .leading)
-                            Spacer()
-                                .frame(minWidth: 50)
+                            
+                            if inEditMode {
+                                
+                                VStack {
+                                    
+                                    Form {
+                                        
+                                        
+                                        TextField("Name", text: $event.name )
+                                        DatePicker("Date", selection: $event.date,
+                                                   displayedComponents: .date)
+                                        HStack {
+                                            Button("Save",action: { updateEvent() })
+                                                .keyboardShortcut(.defaultAction)
+                                            Button("Cancel",action: { inEditMode = false })
+                                                .keyboardShortcut(.cancelAction)
+                                        }
+                                        
+                                        
+                                    }
+                                }
+                                
+                            } else {
+                                
+                                Text(event.name)
+                                    .font(.title)
+                                    .bold()
+                                    .frame(minWidth: 200, alignment: .leading)
+                                    .foregroundColor(fgPrimaryColor)
+                                    .truncationMode(.tail)
+                                    .help(event.name)
+                                Spacer()
+                                Text(event.daysSince)
+                                    .foregroundColor(fgAccentColor)
+                                    .font(.body)
+                                    .frame(minWidth: 100)
+                                
+                            }
+                            
+                            if inEditMode == false {
+                                
+                                Button {
+                                    
+                                    self.inEditMode = true
+                                }
+                                
+                                label: {
+                                    
+                                    Image(systemName: "square.and.pencil")
+                                        .font(.system(size: 14))
+                                        .bold()
+
+                                }
+                                 .help("Edit")
+                                
+                                
+                                Button {
+                                    
+                        
+                                    self.showingDeleteConfirmation = true
+                                    
+                                }
+                                label: {
+                                    
+                                    Image(systemName: "delete.backward")
+                                        .font(.system(size: 14))
+                                        .bold()
+    
+                                }
+                                 .help("Edit")
+                            }
+                            
+                           
                             
                         }
+                        
+                        if inEditMode == false {
+                            
+                            HStack(alignment: .center) {
+                                Text(event.dateValue).foregroundColor(fgPrimaryColor)
+                                    .font(.subheadline)
+                                    .frame(width: 100, alignment: .leading)
+                                Spacer()
+                                    .frame(minWidth: 50)
+                                
+                            }
+                            
+                        }
+                        
+                        
+                        
                         HStack(alignment: .center) {
                             Text(event.id.uuidString).foregroundColor(fgPrimaryColor.opacity(0.5))
                                 .font(.subheadline)
@@ -52,32 +132,85 @@ struct EventView: View {
                         }
                         
                     }
-                    .padding(20)
-                    .frame(width: geometry.size.width - 40,
-                           height: 100, alignment: .leading)
-                    .background(Color.secondary.opacity(0.5)).cornerRadius(10)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.gray, lineWidth: 4)
-                            
-                    )
+                    .padding(10)
+                    .frame(width: geometry.size.width,
+                           height: inEditMode ? 120: 100, alignment: .leading)
+                    .background(Color.white).cornerRadius(10)
+                    .shadow(radius: 2, x: 0, y: 5)
+                    .onTapGesture {
+                        
+                        self.enterEditMode()
+                    }
                     
                     
                 }
                 
                 
             }
-        .padding(.bottom, 45)
+            .scrollDisabled(false)
+            .frame(width: geometry.size.width,
+                   height: 500, alignment: .leading)
+                
+            }
+        .padding(.bottom, 80)
         .padding(.leading, 10)
+        .confirmationDialog("Delete Event", isPresented: $showingDeleteConfirmation) {
+            
+                    
+                    Button("Yes") {
+                       
+                        deleteEvent()
+                    }
+            
+                    Button("No", role: .cancel) {
+                        
+                        showingDeleteConfirmation = false
+                    }
+                
+                    
+        } message: {
+            Text("Do you want to delete the event \"\(event.name)\"")
+        }
         
         
     }
+    
+    func updateEvent() {
         
+        self.inEditMode = false
+        
+        if let controller = controller {
+
+            controller.updateEvent(event: event)
+        }
+    }
+    
+    
+    func deleteEvent() {
+            
+        print("Deleting event with id: \(event.id)")
+        
+            self.showingDeleteConfirmation = false
+            
+            if let controller = controller {
+                controller.deleteEvent(id: event.id)
+            }
+        
+        
+    }
+    
+    func enterEditMode() {
+        
+        // TODO: Preserve previous values in Edit Mode. Right now typing a new Name or selecting a new date will cause the values to be retained in the UI but not written to the database.
+        self.inEditMode = true
+        
+    }
        
 }
 
 struct EventView_Previews: PreviewProvider {
     static var previews: some View {
+        
         EventView(event: Event(name: "Demo Event", date: "10/01/2022"))
 
     }
